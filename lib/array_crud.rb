5b82d362bdb7ade279bacfc921c0f9465dcea14d
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-06-23 03:43:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,6 +6,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array (array, element)
+  adding_to_end_of_array ["test", "problem"]
+  adding_to_end_of_array.push "Arrays!"
 end 
 
 def add_element_to_start_of_array(array, element)
